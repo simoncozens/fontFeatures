dc615495eb1498b7b29d4a7d84d2d0912aa606a8
Simon Cozens <simon@simon-cozens.org>
2020-04-27 13:59:36 +0100

    Bug fix

--- fontFeatures/optimizer/__init__.py
+++ fontFeatures/optimizer/__init__.py
@@ -1,13 +1,13 @@
 from .Routine import optimizations as routine_optimizations
+import fontFeatures
 
 class Optimizer:
   def optimize(self, ff):
     for r in ff.routines:
       self.optimize_routine(r)
-      for k,v in self.features.items():
-        f = feaast.FeatureBlock(k)
+      for k,v in ff.features.items():
         for n in v:
-          if isinstance(n, Routine):
+          if isinstance(n, fontFeatures.Routine):
             self.optimize_routine(n)
 
   def optimize_routine(self, r):
