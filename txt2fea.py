8c33bbabdac444e9e21da8f98f4a56f6884c3caf
Simon Cozens <simon@simon-cozens.org>
2020-05-01 21:51:31 +0100

    Optionally run optimizer

--- txt2fea.py
+++ txt2fea.py
@@ -2,6 +2,7 @@
 import sys
 from fontFeatures.fontDameLib import unparse
 from argparse import ArgumentParser
+from fontFeatures.optimizer import Optimizer
 
 parser = ArgumentParser()
 parser.add_argument("--config",default=None,
@@ -10,8 +11,13 @@ parser.add_argument("input",
                     help="FontDame file to process", metavar="FILE")
 parser.add_argument("--font",
                     help="font file to process", metavar="FONT")
+parser.add_argument("--optimize", dest="optimize", action='store_true',
+                    help="Run optimizer")
 
 args = parser.parse_args()
 
 parsed = unparse(args.input, args.config, font= args.font)
+if args.optimize:
+	Optimizer().optimize(parsed)
+
 print(parsed.asFea())
