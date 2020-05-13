13934e0e0aeaf580527229a1976b50f9aee75df1
Simon Cozens <simon@simon-cozens.org>
2020-05-13 17:15:19 +0100

    Drop yeh barree dots

--- fontFeatures/feeLib/YehBarree.py
+++ fontFeatures/feeLib/YehBarree.py
@@ -19,6 +19,7 @@ class YBFixOverhang:
     binned_medis = bin_glyphs_by_metric(parser.font, medis, "width", bincount = 4)
     binned_inits = bin_glyphs_by_metric(parser.font, inits, "width", bincount = 4)
     rules = []
+    maxchainlength = 0
     for yb in yehbarrees:
       overhang =  -get_glyph_metrics(parser.font,yb)["rsb"]
       workqueue = [[x] for x in binned_inits]
@@ -26,7 +27,8 @@ class YBFixOverhang:
         string = workqueue.pop(0)
         totalwidth = sum([x[1] for x in string])
         if totalwidth > overhang: continue
-        adjustment = overhang - totalwidth + 50
+        maxchainlength = max(maxchainlength, len(string))
+        adjustment = overhang - totalwidth + 20
         postcontext = [ x[0] for x in string[:-1] ] + [ [yb] ]
         input_ = string[-1]
         # example = [x[0] for x in postcontext] + [input_[0][0]]
@@ -41,5 +43,40 @@ class YBFixOverhang:
         )
         for medi in binned_medis:
           workqueue.append([medi] + string)
+    warnings.warn("Yeh Barree collision maximum chain length was %i glyphs" % maxchainlength)
+    return [fontFeatures.Routine(rules=rules)]
 
-    return rules
+class YBDropDots:
+  takesBlock = False
+
+  @classmethod
+  def validate(self, tokens, verbaddress):
+    from fontFeatures.parserTools import ParseError
+    if len(tokens) != 4:
+      raise ParseError("Wrong number of arguments", tokens[0].address, self)
+    return True
+
+  @classmethod
+  def store(self, parser, tokens, doFilter = None):
+    import fontFeatures
+    import itertools
+    from fontFeatures.ftUtils import get_glyph_metrics, bin_glyphs_by_metric
+    import warnings
+    yehbarrees = parser.expandGlyphOrClassName(tokens[0].token)
+    medis = parser.expandGlyphOrClassName(tokens[1].token)
+    dots = parser.expandGlyphOrClassName(tokens[2].token)
+    maxchainlength = int(tokens[3].token)
+    binned_medis = bin_glyphs_by_metric(parser.font, medis, "rise", bincount = 10)
+    rules = []
+    for i in range(0,maxchainlength):
+      for string in itertools.combinations_with_replacement(binned_medis, i):
+        totalrise = sum([ x[1] for x in string])
+        postcontext = [ x[0] for x in string ] + [ yehbarrees ]
+        rules.append(
+          fontFeatures.Positioning(
+            [dots],
+            [fontFeatures.ValueRecord(yPlacement=-(totalrise+300))],
+            postcontext = postcontext
+            )
+        )
+    return [fontFeatures.Routine(rules=rules,flags=0)]
