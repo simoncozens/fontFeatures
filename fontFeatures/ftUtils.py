252feb12434fb47be592634256305b3f57436abe
Simon Cozens <simon@simon-cozens.org>
2020-05-13 17:14:40 +0100

    Determine a glyph’s exit-entry Y distance (“rise”)

--- fontFeatures/ftUtils.py
+++ fontFeatures/ftUtils.py
@@ -39,9 +39,28 @@ def get_glyph_metrics(font, glyphname):
     else:
         bounds = font.getGlyphSet()[glyphname]._glyph.calcBounds(font.getGlyphSet())
         metrics["xMin"], metrics["yMin"], metrics["xMax"], metrics["yMax"] = bounds
+    metrics["rise"] = get_rise(font, glyphname)
     metrics["rsb"] = metrics["width"] - metrics["xMax"]
     return metrics
 
+
+def get_rise(font, glyphname):
+    # Find a cursive positioning feature or it's game over
+    if "GPOS" not in font: return 0
+    t = font["GPOS"].table
+    cursives = filter(lambda x: x.LookupType==3, font["GPOS"].table.LookupList.Lookup)
+    anchors = {}
+    for c in cursives:
+        for s in c.SubTable:
+            for glyph, record in zip(s.Coverage.glyphs, s.EntryExitRecord):
+                anchors[glyph] = []
+                if record.EntryAnchor:
+                    anchors[glyph].append( (record.EntryAnchor.XCoordinate, record.EntryAnchor.YCoordinate) )
+                if record.ExitAnchor:
+                    anchors[glyph].append( (record.ExitAnchor.XCoordinate, record.ExitAnchor.YCoordinate) )
+    if glyphname not in anchors: return 0
+    if len(anchors[glyphname]) == 1: return anchors[glyphname][0][1]
+    return anchors[glyphname][0][1] - anchors[glyphname][1][1]
 
 def bin_glyphs_by_metric(font, glyphs, category, bincount=5):
     metrics = [(g, get_glyph_metrics(font, g)[category]) for g in glyphs]
