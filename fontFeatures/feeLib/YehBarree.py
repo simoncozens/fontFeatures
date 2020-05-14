c975ceddf58a540ad7bc746e7f2031935bfdb6e8
Simon Cozens <simon@simon-cozens.org>
2020-05-14 19:40:30 +0100

    More yeh barree commands

--- fontFeatures/feeLib/YehBarree.py
+++ fontFeatures/feeLib/YehBarree.py
@@ -1,3 +1,30 @@
+class YBSettings:
+  takesBlock = False
+
+  @classmethod
+  def validate(self, tokens, verbaddress):
+    from fontFeatures.parserTools import ParseError
+    if len(tokens) != 2:
+      raise ParseError("Wrong number of arguments", token[0].address, self)
+    return True
+
+  @classmethod
+  def getSettings(self, parser):
+    if "fee-yb-settings" not in parser.fea.scratch:
+      parser.fea.scratch["fee-yb-settings"] = {
+        "OverhangPadding": 20,
+        "DropDotsBinCount": 8
+      }
+    return parser.fea.scratch["fee-yb-settings"]
+
+  @classmethod
+  def store(self, parser, tokens, doFilter = None):
+    keyword = tokens[0].token
+    value   = tokens[1].token
+    self.getSettings(parser)
+    parser.fea.scratch["fee-yb-settings"][keyword] = value
+    return []
+
 class YBFixOverhang:
   takesBlock = False
 
@@ -13,6 +40,7 @@ class YBFixOverhang:
     import fontFeatures
     from fontFeatures.ftUtils import get_glyph_metrics, bin_glyphs_by_metric
     import warnings
+    settings = YBSettings.getSettings(parser)
     yehbarrees = parser.expandGlyphOrClassName(tokens[0].token)
     medis = parser.expandGlyphOrClassName(tokens[1].token)
     inits = parser.expandGlyphOrClassName(tokens[2].token)
@@ -20,6 +48,7 @@ class YBFixOverhang:
     binned_inits = bin_glyphs_by_metric(parser.font, inits, "width", bincount = 4)
     rules = []
     maxchainlength = 0
+    longeststring = []
     for yb in yehbarrees:
       overhang =  -get_glyph_metrics(parser.font,yb)["rsb"]
       workqueue = [[x] for x in binned_inits]
@@ -27,12 +56,12 @@ class YBFixOverhang:
         string = workqueue.pop(0)
         totalwidth = sum([x[1] for x in string])
         if totalwidth > overhang: continue
-        maxchainlength = max(maxchainlength, len(string))
-        adjustment = overhang - totalwidth + 20
+
+        adjustment = overhang - totalwidth + int(settings["OverhangPadding"])
         postcontext = [ x[0] for x in string[:-1] ] + [ [yb] ]
         input_ = string[-1]
-        # example = [x[0] for x in postcontext] + [input_[0][0]]
-        # warnings.warn("Yeh Barree collision found in e.g. %s - adding %i units" % (" ".join(example), adjustment))
+        example = [input_[0][0]] + [x[0] for x in postcontext]
+        maxchainlength = max(maxchainlength, len(string))
 
         rules.append(
           fontFeatures.Positioning(
@@ -44,7 +73,8 @@ class YBFixOverhang:
         for medi in binned_medis:
           workqueue.append([medi] + string)
     warnings.warn("Yeh Barree collision maximum chain length was %i glyphs" % maxchainlength)
-    return [fontFeatures.Routine(rules=rules)]
+    settings["ComputedMaxChainLength"] = maxchainlength
+    return [fontFeatures.Routine(rules=rules, flags=8)]
 
 class YBDropDots:
   takesBlock = False
@@ -52,7 +82,7 @@ class YBDropDots:
   @classmethod
   def validate(self, tokens, verbaddress):
     from fontFeatures.parserTools import ParseError
-    if len(tokens) != 4:
+    if len(tokens) != 3:
       raise ParseError("Wrong number of arguments", tokens[0].address, self)
     return True
 
@@ -62,21 +92,35 @@ class YBDropDots:
     import itertools
     from fontFeatures.ftUtils import get_glyph_metrics, bin_glyphs_by_metric
     import warnings
+    settings = YBSettings.getSettings(parser)
+    if "DropDotsDepth" not in settings:
+      raise ValueError("DropDotsDepth setting is required")
+    if "MaxChainLength" not in settings:
+      if "ComputedMaxChainLength" not in settings:
+        raise ValueError("MaxChainLength setting is required")
+      settings["MaxChainLength"] = settings["ComputedMaxChainLength"]
+
     yehbarrees = parser.expandGlyphOrClassName(tokens[0].token)
     medis = parser.expandGlyphOrClassName(tokens[1].token)
     dots = parser.expandGlyphOrClassName(tokens[2].token)
-    maxchainlength = int(tokens[3].token)
-    binned_medis = bin_glyphs_by_metric(parser.font, medis, "rise", bincount = 10)
+    # XXX check anchors
+    binned_medis = bin_glyphs_by_metric(parser.font, medis, "rise", bincount = int(settings["DropDotsBinCount"]))
     rules = []
-    for i in range(0,maxchainlength):
-      for string in itertools.combinations_with_replacement(binned_medis, i):
-        totalrise = sum([ x[1] for x in string])
-        postcontext = [ x[0] for x in string ] + [ yehbarrees ]
-        rules.append(
-          fontFeatures.Positioning(
-            [dots],
-            [fontFeatures.ValueRecord(yPlacement=-(totalrise+300))],
-            postcontext = postcontext
-            )
-        )
-    return [fontFeatures.Routine(rules=rules,flags=0)]
+    for i in range(0,int(settings["MaxChainLength"])):
+      #XXX This isn't creating all combinations
+      for bases in itertools.combinations_with_replacement(binned_medis, i):
+        for marks in itertools.combinations_with_replacement([dots,None], i):
+          totalrise = sum([ x[1] for x in bases])
+          justbases = [ x[0] for x in bases ]
+          string = [val for pair in zip(justbases, marks) for val in pair]
+          postcontext = [i for i in string if i] + [ yehbarrees ]
+          rules.append(
+            fontFeatures.Positioning(
+              [dots],
+              [fontFeatures.ValueRecord(yPlacement=-(totalrise+int(settings["DropDotsDepth"])))],
+              postcontext = postcontext
+              )
+          )
+    r = fontFeatures.Routine(rules=list(reversed(rules)),flags=0x10 )
+    r.markFilteringSet=dots
+    return [r]
