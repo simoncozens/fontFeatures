354d581ba9c1fb71e403b3b7b676b116d1294171
Simon Cozens <simon@simon-cozens.org>
2020-07-09 12:16:46 +0100

    Read a predefined FEA file full of class definitions

--- txt2fea.py
+++ txt2fea.py
@@ -11,6 +11,8 @@ parser.add_argument("input",
                     help="FontDame file to process", metavar="FILE")
 parser.add_argument("--font",
                     help="font file to process", metavar="FONT")
+parser.add_argument("--classfile",
+                    help="class file to process", metavar="FEA")
 parser.add_argument("--optimize", dest="optimize", action='store_true',
                     help="Run optimizer")
 
@@ -18,6 +20,16 @@ args = parser.parse_args()
 
 parsed = unparse(args.input, args.config, font= args.font)
 optlevel = 1
+if args.classfile:
+  from fontTools.feaLib.parser import Parser
+  parser = Parser(args.classfile)
+  parser.parse()
+  classes = parser.glyphclasses_.scopes_[0]
+  parsed.scratch["glyphclasses"] = {}
+  for k,v in classes.items():
+  	parsed.scratch["glyphclasses"][tuple(sorted(v.glyphs.glyphs))] = k
+  parsed.scratch["index"] = 999
+
 if args.optimize:
 	optlevel = 2
 Optimizer(parsed).optimize(level=optlevel)
