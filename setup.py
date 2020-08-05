ca62a8c8496e1608ff4bf1d19ea94d2b7b25f90a
Simon Cozens <simon@simon-cozens.org>
2020-08-05 15:29:06 +0100

    Requirements etc.

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import os
+
+thelibFolder = os.path.dirname(os.path.realpath(__file__))
+requirementPath = thelibFolder + '/requirements.txt'
+install_requires = []
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
+
+config = {
+    'name': 'fontFeatures',
+    'author': 'Simon Cozens',
+    'author_email': 'simon@simon-cozens.org',
+    'url': 'https://github.com/simoncozens/fontFeatures',
+    'description': 'Python library for manipulating OpenType font features',
+    'long_description': open('README.md', 'r').read(),
+    'license': 'MIT',
+    'version': '0.0.1',
+    'install_requires': install_requires,
+    'classifiers': [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 4 - Beta"
+
+    ],
+    'package_dir': {'fontFeatures': 'fontFeatures'},
+    'packages': find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
+,
+}
+
+if __name__ == '__main__':
+    setup(**config)
