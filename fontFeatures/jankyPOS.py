a7a2538e4cd47d3d384a079db654d3f2aaf1d9e5
Simon Cozens <simon@simon-cozens.org>
2020-08-05 15:20:09 +0100

    Some fixes to jankypos, but it’s still not quite working.

--- fontFeatures/jankyPOS.py
+++ fontFeatures/jankyPOS.py
@@ -1,5 +1,6 @@
-from fontFeatures.ftUtils import get_glyph_metrics
+from fontFeatures.ftUtils import get_glyph_metrics, categorize_glyph
 from fontFeatures import ValueRecord, Attachment, Positioning, Chaining, Routine
+import warnings
 
 
 def add_value_records(vr1, vr2):
@@ -18,11 +19,25 @@ class JankyPos:
         self.font = font
         self.direction = direction
 
+
+    def serialize_buffer(self, buf):
+        """Returns the contents of the given buffer in a string format similar to
+    that used by hb-shape."""
+        outs = []
+        for info in buf:
+            position = info["position"]
+            outs.append("%s" % info["glyph"])
+            outs[-1] = outs[-1] + "+%i" % position.xAdvance
+            if position.xPlacement != 0 or position.yPlacement != 0:
+                outs[-1] = outs[-1] + "@<%i,%i>" % (position.xPlacement or 0, position.yPlacement or 0)
+        return "|".join(outs)
+
     def positioning_buffer(self, glyphstring):
-        if self.direction == "RTL":
-            glyphstring = list(reversed(glyphstring))
         return [
-            (g, ValueRecord(xAdvance=get_glyph_metrics(self.font, g)["width"]))
+            { "glyph": g,
+              "position": ValueRecord(xAdvance=get_glyph_metrics(self.font, g)["width"]),
+              "category": categorize_glyph(self.font, g)
+            }
             for g in glyphstring
         ]
 
@@ -47,6 +62,8 @@ class JankyPos:
                     buf = self.process_rules(buf, r.rules)
                 else:
                     buf = self.process_rules(buf, [r])
+        if self.direction == "RTL":
+            buf = list(reversed(buf))
         return buf
 
     def process_rules(self, buf, rules):
@@ -70,10 +87,10 @@ class JankyPos:
         )
         assert len(rule.glyphs) == 1
         for i in applicable_range:
-            g, vr = buf[i]
+            g, vr = buf[i]["glyph"], buf[i]["position"]
             if rule.precontext or rule.postcontext:
-                pre = [x[0] for x in buf[i - len(rule.precontext) + 1 : i]]
-                post = [x[0] for x in buf[i + 1 : i + len(rule.postcontext) + 1]]
+                pre = [x["glyph"] for x in buf[i - len(rule.precontext) + 1 : i]]
+                post = [x["glyph"] for x in buf[i + 1 : i + len(rule.postcontext) + 1]]
                 if tuple(pre) != tuple(rule.precontext) or tuple(post) != tuple(
                     rule.postcontext
                 ):
@@ -84,18 +101,30 @@ class JankyPos:
         return buf
 
     def attach(self, buf, rule):
-        for ix, (g, vr) in enumerate(buf):
+        for ix, info in enumerate(buf):
+            g = info["glyph"]
+            vr = info["position"]
             if ix == 0:
                 continue
             # XXX search backwards until you find a base
-            prev = buf[ix - 1][0]
+            # XXX Unless we are doing mkmk
+            previous = ix - 1
+            while previous > 0 and buf[previous]["category"][0] != "base":
+                previous = previous - 1
+            prev = buf[previous]["glyph"]
+            prevVr = buf[previous]["position"]
+            print("Trying %s(%i)/%s" % (g, ix, prev))
             if g in rule.marks and ix > 0 and prev in rule.bases:
-                prevVr = buf[ix - 1][1]
-                xpos = rule.bases[prev][0] - rule.marks[g][0]
-                ypos = rule.bases[prev][1] - rule.marks[g][1]
+                print("Attach %s -> %s" % ( g, prev))
+                xpos = -(rule.bases[prev][0] - rule.marks[g][0])
+                ypos = -(rule.bases[prev][1] - rule.marks[g][1])
+                print(" %i, %i" % ( xpos, ypos))
                 if rule.is_cursive:
                     xpos = xpos + (prevVr.xPlacement or 0)
                     ypos = ypos + (prevVr.yPlacement or 0)
-                vr.xPlacement = (vr.xPlacement or 0) + xpos - prevVr.xAdvance
-                vr.yPlacement = (vr.yPlacement or 0) + ypos
+                    prevVr.xPlacement = (prevVr.xPlacement or 0) + xpos - prevVr.xAdvance
+                    prevVr.yPlacement = (prevVr.yPlacement or 0) + ypos
+                else:
+                    vr.xPlacement = (vr.xPlacement or 0) + xpos - prevVr.xAdvance
+                    vr.yPlacement = (vr.yPlacement or 0) + ypos
         return buf
