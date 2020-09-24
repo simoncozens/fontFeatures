aee410842a3025fa116ce1f5f1c9d1aa9151fa0d
Simon Cozens <simon@simon-cozens.org>
2020-09-24 12:58:53 +0100

    Substitution fixes

--- fontFeatures/shaperLib/Substitution.py
+++ fontFeatures/shaperLib/Substitution.py
@@ -1,3 +1,5 @@
+import copy
+
 def shaper_inputs(self):
     return self.input
 
@@ -6,6 +8,17 @@ def _do_apply(self, buf, ix):
     from fontFeatures.jankyPOS.Buffer import BufferItem
 
     coverage = buf[ix : ix + len(self.input)]
-    buf[ix : ix + len(self.input)] = [
-        BufferItem.new_glyph(g, buf.font) for g in self.replacement
-    ]
+    newstuff = []
+    # Handle single subst first
+    if len(self.input) == 1 and len(self.replacement) == 1:
+        buf[ix].glyph = self.replacement[0][self.input[0].index(buf[ix].glyph)]
+        buf[ix].prep_glyph(buf.font)
+        return
+
+    for g in self.replacement:
+        new_glyph = copy.copy(buf[ix])
+        new_glyph.glyph = g[0]
+        new_glyph.prep_glyph(buf.font)
+        print(g[0], new_glyph.position.asFea())
+        newstuff.append(new_glyph)
+    buf[ix : ix + len(self.input)] = newstuff
