185d4a147e451db10b269423335d2e945363f15a
Simon Cozens <simon@simon-cozens.org>
2020-09-24 12:58:07 +0100

    Call pause functions

--- fontFeatures/shaperLib/BaseShaper.py
+++ fontFeatures/shaperLib/BaseShaper.py
@@ -55,7 +55,7 @@ class BaseShaper():
                 r.apply_to_buffer(self.buffer, current_stage)
         else:
             # It's a pause
-            pass
+            stage(current_stage)
 
   def hide_default_ignorables(self):
     pass
