fc195ef0692d83c005a233903f66c010ec868fa8
Simon Cozens <simon@simon-cozens.org>
2020-11-13 09:16:44 +0000

    Change logging

--- fontFeatures/shaperLib/Routine.py
+++ fontFeatures/shaperLib/Routine.py
@@ -9,13 +9,12 @@ def apply_to_buffer(self, buf, stage=None, feature=None):
         buf.set_feature_mask(feature)
     i = 0
     while i < len(buf): # (which may change!)
-        logging.getLogger("fontFeatures.shaperLib").debug("\nPosition %i\n" % (i))
         for r in self.rules:
             if stage and r.stage != stage:
                 continue
             buf.set_mask(r.flags, self.markFilteringSet, self.markAttachmentSet)
             if r.would_apply_at_position(buf, i):
-                logging.getLogger("fontFeatures.shaperLib").info("Applying rule %s at position %i\n" % (r.asFea(), i))
+                logging.getLogger("fontFeatures.shaperLib").debug("Applying rule %s at position %i\n" % (r.asFea(), i))
                 delta = r._do_apply(buf, i)
                 buf.update()
                 if delta:
