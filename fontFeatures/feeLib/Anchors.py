9d480c402caa325deb6d669502b913439d305ba1
Simon Cozens <simon@simon-cozens.org>
2021-01-05 14:31:07 +0000

    Rename dump-anchors to dump cursive, add dump-anchors from Anchors

--- fontFeatures/feeLib/Anchors.py
+++ fontFeatures/feeLib/Anchors.py
@@ -105,25 +105,3 @@ class Attach:
             )
         ]
 
-
-# Dumping this here - script to export Anchors FEE from a FontFeatures object
-# import itertools
-# flatten = itertools.chain.from_iterable
-# mark = list(flatten([x.rules for x in parsed.features["mark"]]))
-# mkmk = list(flatten([x.rules for x in parsed.features["mkmk"]]))
-# allfeat = mark + mkmk
-# for f in allfeat:
-#     for b in f.bases.keys():
-#         if not b in parsed.anchors:
-#             parsed.anchors[b] = {}
-#         parsed.anchors[b][f.base_name] = f.bases[b]
-#     for b in f.marks.keys():
-#         if not b in parsed.anchors:
-#             parsed.anchors[b] = {}
-#         parsed.anchors[b][f.mark_name] = f.marks[b]
-
-# for glyph in parsed.anchors.keys():
-#     print("Anchors %s {" % glyph)
-#     for name, anchor in parsed.anchors[glyph].items():
-#         print("\t%s <%i %i>" % (name, *anchor))
-#     print("};")
--- utils/dump-anchors.py
+++ utils/dump-anchors.py
@@ -1,91 +1,34 @@
-# Parse a GPOS ttx dump and output a cursive attachment .fea
-import xml.etree.cElementTree as et
-from fontTools.feaLib.ast import *
+import itertools
+from fontFeatures.ttLib import unparse
+from argparse import ArgumentParser
+from fontTools.ttLib import TTFont
 
-tree = et.parse("cursive.xml")
-doc = tree.getroot()
-glyphs = [x.attrib["value"] for x in tree.findall("Coverage/Glyph")]
-recs = tree.findall("EntryExitRecord")
 
-droplist = []
-poslist = []
-for i,rec in enumerate(recs):
-  entries = rec.findall("EntryAnchor")
-  exits   = rec.findall("ExitAnchor")
-  if len(entries) > 0:
-    entryY = int(entries[0].find("YCoordinate").attrib["value"])
-    entryX = int(entries[0].find("XCoordinate").attrib["value"])
-    entryAnchor = Anchor(entryX, entryY)
-  else:
-    entryY = 0
-    entryAnchor = Anchor(0,0,name="NULL")
+parser = ArgumentParser()
+parser.add_argument("input", help="font file to process", metavar="FILE")
+args = parser.parse_args()
+font = TTFont(args.input)
+parsed = unparse(font)
 
-  if len(exits) > 0:
-    exitY = int(exits[0].find("YCoordinate").attrib["value"])
-    exitX = int(exits[0].find("XCoordinate").attrib["value"])
-    exitAnchor = Anchor(exitX, exitY)
-  else:
-    exitY = 0
-    exitAnchor = Anchor(0,0,name="NULL")
+flatten = itertools.chain.from_iterable
+mark = list(flatten([x.rules for x in parsed.features["mark"]]))
+mkmk = list(flatten([x.rules for x in parsed.features["mkmk"]]))
+allfeat = mark + mkmk
 
-  drop = entryY - exitY
-  droplist.append((glyphs[i],drop))
-  poslist.append(CursivePosStatement(GlyphName(glyphs[i]), entryAnchor, exitAnchor))
+import code; code.interact(local=locals())
 
-droplist = sorted(droplist, key = lambda x:x[1])
+for f in allfeat:
+    for b in f.bases.keys():
+        if not b in parsed.anchors:
+            parsed.anchors[b] = {}
+        parsed.anchors[b][f.base_name] = f.bases[b]
+    for b in f.marks.keys():
+        if not b in parsed.anchors:
+            parsed.anchors[b] = {}
+        parsed.anchors[b][f.mark_name] = f.marks[b]
 
-import numpy as np
-
-def groupByDrop(droplist, classname, bincount=10):
-  relevant = list(filter(lambda x: classname in x[0] , droplist))
-  dropValues = [ x[1] for x in relevant ]
-  edges = np.histogram_bin_edges(dropValues, bins = bincount)
-  middles = list((edges[0:-1] + edges[1:] ) /2)
-  groups = np.digitize(dropValues, edges) -1
-
-  binnedGlyphs = {}
-  for i,group in enumerate(groups):
-    if not group in binnedGlyphs:
-      binnedGlyphs[group] = []
-    binnedGlyphs[group].append(relevant[i][0])
-  middles.append(edges[-1])
-  classes = []
-  for k,v in binnedGlyphs.items():
-    classes.append(GlyphClassDefinition("%s_%i" % (classname,k), GlyphClass(v)))
-    classes.append(Comment("# average drop = %i\n" % middles[k]))
-
-  return binnedGlyphs, middles, classes
-
-binnedMeds, medMiddles, medClasses = groupByDrop(droplist, "Med")
-for x in medClasses:
-  print(x.asFea())
-binnedInis, iniMiddles, iniClasses = groupByDrop(droplist, "Ini")
-for x in iniClasses:
-  print(x.asFea())
-
-binnedFins, finMiddles, finClasses = groupByDrop(droplist, "Fin")
-for x in finClasses:
-  print(x.asFea())
-
-print("feature curs {\n")
-print("lookupflag IgnoreMarks;\n")
-for x in poslist:
-  print(x.asFea())
-print("} curs;")
-
-# print("feature kern {\n")
-# print("lookupflag IgnoreMarks;\n")
-
-# for i in binnedInis.keys():
-#   for m1 in binnedMeds.keys():
-#     for f in binnedFins.keys():
-#       totalDrop = (iniMiddles[i] + medMiddles[m1] + finMiddles[f])
-#       print("\tposition @Ini_%i' <0 %i 0 0> @Med_%i @Fin_%i;" % (i, totalDrop, m1,f))
-
-# for i in binnedInis.keys():
-#   for m1 in binnedMeds.keys():
-#     for m2 in binnedMeds.keys():
-#       for f in binnedFins.keys():
-#         totalDrop = (iniMiddles[i] + medMiddles[m1] + medMiddles[m2] + finMiddles[f])
-#         print("\tposition @Ini_%i' <0 %i 0 0> @Med_%i  @Med_%i  @Fin_%i;" % (i, totalDrop, m1,m2,f))
-#   print("} kern;")
+for glyph in parsed.anchors.keys():
+    print("Anchors %s {" % glyph)
+    for name, anchor in parsed.anchors[glyph].items():
+        print("\t%s <%i %i>" % (name, *anchor))
+    print("};")
