17ef43f275474ee23036a51555ed92288e2c14f0
Simon Cozens <simon@simon-cozens.org>
2021-01-21 14:21:43 +0000

    Bump version: 1.0.0 → 1.0.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ config = {
     'description': 'Python library for manipulating OpenType font features',
     'long_description': open('README.md', 'r').read(),
     'license': 'MIT',
-    'version': '1.0.0',
+    'version': '1.0.1',
     'install_requires': install_requires,
     'classifiers': [
         "Programming Language :: Python",
