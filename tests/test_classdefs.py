157b496371da85127c12097c593e4a8c215e9972
Fredrick Brennan <copypaste@kittens.ph>
2021-02-04 08:57:03 -0500

    Add test for Unicode ranges

--- tests/test_classdefs.py
+++ tests/test_classdefs.py
@@ -6,6 +6,8 @@ import unittest
 import re
 
 class TestFeeDefinitions(unittest.TestCase):
+    roboto = FeeParser(Babelfont.load("fonts/Roboto-Regular.ttf"))
+
     def assertSufficientlyEqual(self, s1, s2):
         def alltrim(a):
             a = re.sub("#.*", "", a)
@@ -15,10 +17,13 @@ class TestFeeDefinitions(unittest.TestCase):
         self.assertEqual(alltrim(s1), alltrim(s2))
 
     def test_parse_to_ff(self):
-        p = FeeParser(Babelfont.load("fonts/Roboto-Regular.ttf"))
-        p.parseString("""
+        self.roboto.parseString("""
         	DefineClass @vowels = /^[aeiou]$/;
         	DefineClass @consonants = /^[bcdfghjklmnpqrstvwxyz]$/;
         	DefineClass @letters = [@vowels @consonants];
         	""");
-        self.assertEqual(len(p.fontfeatures.namedClasses["letters"]),26)
+        self.assertEqual(len(self.roboto.fontfeatures.namedClasses["letters"]),26)
+
+    def test_unicode_range_selector(self):
+        self.roboto.parseString("DefineClass @digits = U+0030=>U+0039;");
+        self.assertEqual(len(self.roboto.fontfeatures.namedClasses["digits"]),10)
