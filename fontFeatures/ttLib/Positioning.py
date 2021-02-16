f7adf831d124654e064df411b9fc3ab8a1d58937
Simon Cozens <simon@simon-cozens.org>
2021-02-16 22:41:58 +0000

    has_context simplifies this dramatically

--- fontFeatures/ttLib/Positioning.py
+++ fontFeatures/ttLib/Positioning.py
@@ -1,12 +1,8 @@
 def lookup_type(self):
-    if (not self.precontext or len(self.precontext) == 0) and (
-        not self.postcontext or len(self.postcontext) == 0
-    ):
+    if not self.has_context:
         if len(self.glyphs) == 1:
             return 1
         if len(self.glyphs) == 2:
             return 2
-    else:
-    		if len(self.glyphs) == 1:
-	    			return 1
-    return 8  # ???
+    return 7
+
