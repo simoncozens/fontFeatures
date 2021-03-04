1cdfc4051016f0d5fe558a26f2ecbde0dc13843b
Simon Cozens <simon@simon-cozens.org>
2021-03-04 10:37:12 +0000

    An attachment rule is soon going to need to know where it came from

--- fontFeatures/ttLib/Attachment.py
+++ fontFeatures/ttLib/Attachment.py
@@ -4,6 +4,10 @@ from glyphtools import categorize_glyph
 def lookup_type(self):
     if self.is_cursive:
         return 3
-    if self.font and categorize_glyph(self.font,base[0][0])[0] == "mark":
+    # Terrible hacks
+    firstbase = list(self.bases.keys())[0]
+    if hasattr(self, "fontfeatures") and self.fontfeatures.glyphclasses.get(firstbase) == "mark":
+        return 6
+    if self.font and categorize_glyph(self.font,firstbase)[0] == "mark":
         return 6
     return 4
