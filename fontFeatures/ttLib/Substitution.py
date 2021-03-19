9bee77499cfbdc0ed8bbbc97a315472e61cf93bc
Simon Cozens <simon@simon-cozens.org>
2021-03-19 15:44:44 +0000

    We can express contextual lookups as type 5.

--- fontFeatures/ttLib/Substitution.py
+++ fontFeatures/ttLib/Substitution.py
@@ -3,7 +3,7 @@ def lookup_type(self):
         return 8
     if not self.replacement:
         return 2 # Deletion but treat as mult sub
-    if len(self.lookups) > 0 and any([x is not None for x in self.lookups]) or self.has_context:
+    if len(self.lookups) > 0 and any([x is not None for x in self.lookups]):
         return 6  # Chaining
     # if self.input == self.replacement: # It's an ignore
     # return 6
