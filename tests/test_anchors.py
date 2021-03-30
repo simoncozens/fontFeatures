ad0a2b7e9e3ce2e85f0821073d3b68abed147772
Simon Cozens <simon@simon-cozens.org>
2021-03-30 15:07:28 +0100

    Stop tests being weird on CI

--- tests/test_anchors.py
+++ tests/test_anchors.py
@@ -1,7 +1,19 @@
 from fontFeatures import Attachment, FontFeatures
 from lxml import etree
+import re
 
 import unittest
+
+def assertSufficientlyEqual(s1, s2):
+    def alltrim(a):
+        a = re.sub("lookupflag 0;", "", a)
+        a = re.sub("#.*", "", a)
+        a = re.sub("\\s+", " ", a)
+        a = re.sub("table GDEF.*GDEF;", "", a)
+        a = re.sub(" ; ", " ", a)
+        return a.strip()
+
+    assert alltrim(s1) == alltrim(s2)
 
 
 class TestAnchors(unittest.TestCase):
@@ -11,7 +23,7 @@ class TestAnchors(unittest.TestCase):
 
     def test_markbase(self):
         s = Attachment("top", "top_", {"A": (679, 1600)}, {"acutecomb": (-570, 1290)})
-        self.assertEqual(s.asFea(), "    pos base A <anchor 679 1600> mark @top;\n")
+        assertSufficientlyEqual(s.asFea(), "    pos base A <anchor 679 1600> mark @top;\n")
         self.assertEqual(s.involved_glyphs, set(["A", "acutecomb"]))
         self.assertEqual(etree.tostring(s.toXML()), '<attachment basename="top" markname="top_"><base name="A" anchorX="679" anchorY="1600"/><mark name="acutecomb" anchorX="-570" anchorY="1290"/></attachment>'.encode("utf-8"))
         self.roundTrip(s)
