d84359016019a845bf40f13d7afdb9adf813f374
Simon Cozens <simon@simon-cozens.org>
2021-10-19 13:53:21 +0100

    Bump version: 1.1.2 → 1.2.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ config = {
     'long_description': open('README.md', 'r').read(),
     'long_description_content_type': 'text/markdown',
     'license': 'MIT',
-    'version': '1.1.2',
+    'version': '1.2.0',
     'install_requires': install_requires,
     'extras_require': {
         'shaper': [
