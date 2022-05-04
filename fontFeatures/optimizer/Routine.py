b27dcae0973a511c57640bd062d844cde532a73b
Simon Cozens <simon@simon-cozens.org>
2022-05-04 11:38:56 +0100

    Optimization to ensure chain lookups can be expressed as format 2

--- fontFeatures/optimizer/Routine.py
+++ fontFeatures/optimizer/Routine.py
@@ -1,5 +1,7 @@
-from fontFeatures import Substitution, Positioning
+from fontTools.otlLib.builder import ClassDefBuilder
+from fontFeatures import Substitution, Positioning, Chaining
 import logging
+import copy
 
 
 class MoveLongCoverageToClassDefinition:
@@ -89,8 +91,67 @@ class MergeMultipleSingleSubstitutions:
             postcontext=first.postcontext,
         )
 
+class EnsureFormat2Chaining:
+    level = 1
+
+    def apply(self, routine, ff):
+        rules = routine.rules
+        if not all(isinstance(rule,Chaining) for rule in routine.rules):
+            return
+
+        while True:
+            if self._apply(rules):
+                break
+
+    def _apply(self, rules):
+        any_failing = False
+        logger = logging.getLogger("fontFeatures")
+        logger.warn("Applying format 2 optimization round")
+
+        for which in ["input", "precontext", "postcontext"]:
+            classdefbuilder = ClassDefBuilder(useClass0=False)
+            for rule in rules:
+                for slot in getattr(rule, which):
+                    if not classdefbuilder.canAdd(set(slot)):
+                        logger.warn("Mitigating. Rule count before=%i", len(rules))
+                        self.mitigate(rules, which, classdefbuilder.classes(), set(slot))
+                        logger.warn("Mitigating. Rule count after=%i", len(rules))
+                        return False
+                    classdefbuilder.add(set(slot))
+        return True
+
+    def mitigate(self, rules, which, rule_classes, failing_slot):
+        # What kind of fail is this?
+        problem = None
+        for klass in rule_classes[1:]:
+            if not set(klass).isdisjoint(failing_slot):
+                problem = klass
+                break
+        # Split both into two and try again
+        additional_rules = []
+        intersection = set(problem) & failing_slot
+        assert intersection
+        for rule in rules:
+            for ix, slot in enumerate(getattr(rule, which)):
+                if (set(slot) == set(problem) or set(slot) == failing_slot) and set(slot) != intersection:
+                    new_rule = Chaining(
+                        copy.deepcopy(rule.input),
+                        precontext=copy.deepcopy(rule.precontext),
+                        postcontext=copy.deepcopy(rule.postcontext),
+                        lookups=rule.lookups[:]
+                    )
+                    setattr(new_rule, which, getattr(new_rule, which)[:])
+                    setattr(rule, which, getattr(rule, which)[:])
+                    getattr(rule, which)[ix] = list(set(slot) - intersection)
+                    getattr(new_rule, which)[ix] = list(intersection)
+                    additional_rules.append(new_rule)
+        rules.extend(additional_rules)
+
+
+
 
 optimizations = [
     MergeMultipleSingleSubstitutions,
+    EnsureFormat2Chaining,
     MoveLongCoverageToClassDefinition,  # Runs last
 ]
